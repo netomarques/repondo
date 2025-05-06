10fbbd990330d170f7fa78a746775ee999e6fba4
Wilson Neto <wilsonmarquesg12@gmail.com>
2025-05-06 16:28:14 -0400

    feat(splash): implementar splash screen com animação e assets de fundo
    Adicionada a tela de splash com animação.
    Integrados os assets de fundo para melhorar a experiência visual ao iniciar o app.

--- lib/features/auth/presentation/pages/splash_page.dart
+++ lib/features/auth/presentation/pages/splash_page.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends ConsumerStatefulWidget {
   static SplashPage builder(BuildContext context, GoRouterState state) =>
       const SplashPage();
 
   const SplashPage({super.key});
 
   @override
+  ConsumerState<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends ConsumerState<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    );
+
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+
+    _controller.forward();
+
+    // Simular o delay e alteração de estado
+    Future.delayed(const Duration(seconds: 3), () {});
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: CircularProgressIndicator()),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: Image.asset(
+              'assets/logo.png', // Coloque o caminho para o seu logo
+              // width: 200,
+              // height: 200,
+              fit: BoxFit.cover),
+        ),
+      ),
     );
   }
 }
